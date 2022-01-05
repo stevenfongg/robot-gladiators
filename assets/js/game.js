08ddea89aef5337d6cdcdcd690ea31ef50386f51
Steven Fong <stevenfong98@gmail.com>
2022-01-04 19:00:51 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,9 +5,16 @@ var playerAttack = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames= ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  };
+
 
 var playerMoney = 10;
 
@@ -29,7 +36,7 @@ var fight = function() {
 
         // Log a resulting message to the console so we know that it worked.
         console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+        playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining.");
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
         playerHealth = playerHealth - enemyAttack;
 
@@ -40,13 +47,13 @@ var fight = function() {
         }
 
         // Log a resulting message to the console so we know that it worked.
-        console.log( enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        console.log( enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
         // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         } 
         else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         } 
     } else if(promptFight === "skip" || promptFight === "SKIP"){
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
@@ -58,4 +65,6 @@ var fight = function() {
         }
     }
 }
-fight();
+for(var i=0; i< enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
